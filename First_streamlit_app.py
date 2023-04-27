933fe5d24002d27963a63cb9d4c02b3325a418f0
Thavvasneha <131946027+Thavvasneha@users.noreply.github.com>
2023-04-27 16:03:33 +0530

    Update First_streamlit_app.py

--- First_streamlit_app.py
+++ First_streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
 
-streamlit.title('LETS HAVE HEALTHY LUNCH')
-
-streamlit.header('menu for LUNCH')
-
-streamlit.text('Roti')
-streamlit.text('curry')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
