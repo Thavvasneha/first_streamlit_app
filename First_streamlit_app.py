760106a9150b581d1702aa5fb04255c77c035a52
Thavvasneha <131946027+Thavvasneha@users.noreply.github.com>
2023-04-27 16:01:50 +0530

    Update First_streamlit_app.py

--- First_streamlit_app.py
+++ First_streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('I am happy')
+streamlit.title('LETS HAVE HEALTHY LUNCH')
+
+streamlit.header('menu for LUNCH')
+
+streamlit.text('Roti')
+streamlit.text('curry')
+
